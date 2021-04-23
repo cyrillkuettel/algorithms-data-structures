592ea02d1f83fc7656c4f978ded4fa968bb37ac4
Cyrill <cyrillkuettel@gmail.com>
2021-04-23 13:39:07 +0200

    messung works

--- src/main/java/ch/hslu/sw08/Laufzeitmessung.java
+++ src/main/java/ch/hslu/sw08/Laufzeitmessung.java
@@ -11,31 +11,47 @@ import org.apache.logging.log4j.Logger;
  */
 public final class Laufzeitmessung {
 
-    private static final Logger log = LogManager.getLogger(JDrawCircle.class);
-    private int SIZE = 100000;
+    private static final Logger log = LogManager.getLogger(Laufzeitmessung.class);
+    private int SIZE;
+    private int[] a;
 
-    public Laufzeitmessung(final Sortieren sortierer, final int size) {
+    public Laufzeitmessung(final int size) {
         this.SIZE = size;
+
+        log.info("Generating Random Sequence");
         RandomArrayGenerator rnd = new RandomArrayGenerator();
-        int[] a = rnd.generateUniqueRandomArray(SIZE);
-        log.info("Starting Sort ...");
-        log.info(Arrays.toString(a).substring(0, 30));
-        
+
+        this.a = rnd.generateUniqueRandomArray(SIZE);
+
+    }
+
+    public void messung(final Sortieren sortierer) {
+        int[] a = this.a.clone();
+        log.info("Starting {} ...", (sortierer.getClass().getSimpleName()));
+        log.info(Arrays.toString(a).substring(0, 20));
+
         long start = System.currentTimeMillis();
         sortierer.sort(a);
         long end = System.currentTimeMillis();
-        
-        log.info(Arrays.toString(a).substring(0, 30));
+
+        log.info(Arrays.toString(a).substring(0, 20));
         System.out.format("Finished sorting. Total time: %d ms", end - start);
         System.out.println();
     }
 
     public static void main(String[] args) {
-        
-        new Laufzeitmessung(new InsertionSort(), 100000);
-        new Laufzeitmessung(new SelectionSort(), 100000);
-        new Laufzeitmessung(new BubbleSort(), 100000);
-        
+//        Laufzeitmessung m = new Laufzeitmessung(100000);
+//
+//        m.messung(new InsertionSort());
+//        m.messung(new SelectionSort());
+//        m.messung(new BubbleSort());
+
+        Laufzeitmessung m2 = new Laufzeitmessung(200000);
+
+        m2.messung(new InsertionSort());
+        m2.messung(new SelectionSort());
+        m2.messung(new BubbleSort());
+
     }
 
 }
