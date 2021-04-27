f2aabc5f6817959a959fd0283e5ea398fd84b559
Cyrill <cyrillkuettel@gmail.com>
2021-04-27 11:20:59 +0200

    Deletion of max element

--- src/main/java/ch/hslu/ad/sw09/FixedSizeHeap.java
+++ src/main/java/ch/hslu/ad/sw09/FixedSizeHeap.java
@@ -1,6 +1,6 @@
 package ch.hslu.ad.sw09;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -21,19 +21,15 @@ public class FixedSizeHeap implements IntegerHeap {
 
     public static void main(String[] args) {
         FixedSizeHeap heap = new FixedSizeHeap(30);
+
+        heap.insert(9);
         heap.insert(4);
         heap.insert(7);
-        heap.insert(9);
-        heap.insert(2);
-        heap.insert(3);
-        heap.insert(45);
-        heap.insert(32);
-        heap.insert(33);
-        heap.insert(85);
-        heap.insert(34);
-        heap.insert(1);
-
         heap.printHeap();
+//        heap.printArray();
+
+        heap.getMax();
+        heap.printArray();
 
     }
 
@@ -42,12 +38,16 @@ public class FixedSizeHeap implements IntegerHeap {
         heap[size] = element;
         size++;
 
-        int currentIndex = size-1; 
-        while (heap[currentIndex] > heap[parent(currentIndex)]) {
+        int currentIndex = size - 1;
+        while (heap[currentIndex] > heap[parent(currentIndex)]) { // reorganzie up
             swap(currentIndex, parent(currentIndex));
             currentIndex = parent(currentIndex);
         }
 
+    }
+
+    public void printArray() {
+        System.out.println(Arrays.toString(heap));
     }
 
     private void printHeap() {
@@ -96,9 +96,44 @@ public class FixedSizeHeap implements IntegerHeap {
         System.out.println(hs.toString());
     }
 
-    // after the Max element is popped out, it is neccessary to rearrange the structure
-    public void reArrangeTopDown() {
+    // after the Max element is popped out, it may be neccessary to rearrange the structure
+    public void reArrangeTopDown(int index) {
 
+        if (noChildrenBigger(index)) {
+            return; // Dann ist ausgeheapt
+        } else {
+            System.out.println("got here");
+            int BiggerChildIndex = getBiggerChild(index);
+            swap(BiggerChildIndex, index);
+            reArrangeTopDown(BiggerChildIndex); // or is it: get Bigger Child
+        }
+
+    }
+
+    public boolean noChildrenBigger(int index) {
+        int parent = heap[index];
+        return heap[leftChild(index)] <= parent && heap[rightChild(index)] <= parent;
+
+    }
+
+    private int getBiggerChild(int parent) {
+
+        if (leftChild(parent) >= rightChild(parent)) {
+            return leftChild(parent);
+        } else {
+            return rightChild(parent);
+        }
+    }
+
+    public int getMax() {
+        int max = heap[0];
+        printArray();
+        swap(0, size - 1); // swap the root with last Index
+        size--;
+        heap[size] = 0;
+        printArray();
+        reArrangeTopDown(0);
+        return max;
     }
 
     private boolean isLeaf(int pos) {
@@ -113,6 +148,14 @@ public class FixedSizeHeap implements IntegerHeap {
         return (index - 1) / 2;
     }
 
+    private int rightChild(int index) {
+        return 2 * (index + 1);
+    }
+
+    private int leftChild(int index) {
+        return (index * 2) + 1;
+    }
+
     @Override
     public void remove(int inp) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -120,12 +163,12 @@ public class FixedSizeHeap implements IntegerHeap {
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return size == 0;
     }
 
     @Override
     public boolean isFull() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return size == MAX_SIZE;
     }
 
     public void swap(int i, int j) {
